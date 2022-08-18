a648505e65c0074f9d4b46839d10b822b8260d1a
yianzhou <2873642764@qq.com>
2022-08-18 17:14:53 +0800

    User的mapper文件

---
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.atguigu.mybatisplus.mapper;
+
+import com.atguigu.mybatisplus.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-08-18-14:50
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
