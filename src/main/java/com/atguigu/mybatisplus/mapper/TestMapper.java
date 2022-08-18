cf836ad113d9dad0c918531ba43d1c70e7e23779
yianzhou <2873642764@qq.com>
2022-08-18 17:13:53 +0800

    Test的mapper文件

---
+++ src/main/java/com/atguigu/mybatisplus/mapper/TestMapper.java
@@ -0,0 +1,19 @@
+package com.atguigu.mybatisplus.mapper;
+
+import com.atguigu.mybatisplus.pojo.User;
+
+import java.util.List;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-08-18-15:03
+ */
+public interface TestMapper {
+    /**
+     * 查询所有用户信息
+     * @return
+     */
+    List<User> getAllUser();
+}
