ca6142736df404b30838dc46f500c0d42ccb222f
yianzhou <2873642764@qq.com>
2022-08-21 14:09:29 +0800

    userService的java文件

---
+++ src/main/java/com/atguigu/mybatis/mybatisplus/service/UserService.java
@@ -0,0 +1,14 @@
+package com.atguigu.mybatis.mybatisplus.service;
+
+import com.atguigu.mybatis.mybatisplus.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-08-19-14:40
+ */
+public interface UserService extends IService<User> {
+
+}
