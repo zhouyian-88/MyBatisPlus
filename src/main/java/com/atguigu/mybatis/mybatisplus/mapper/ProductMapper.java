6a02c37de4dcc065008b419cdd49eb0ce61edd1c
yianzhou <2873642764@qq.com>
2022-08-21 14:06:22 +0800

    product的mapper类

---
+++ src/main/java/com/atguigu/mybatis/mybatisplus/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.atguigu.mybatis.mybatisplus.mapper;
+
+import com.atguigu.mybatis.mybatisplus.pojo.Product;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-08-20-23:17
+ */
+@Repository
+public interface ProductMapper extends BaseMapper<Product> {
+}
